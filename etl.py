eb2e183d5ca75faa476059ed6f5a1d54df035cee
DiegoFreitasDS <diegop.freitas@gmail.com>
2024-05-23 00:05:20 -0300

    Pipeline ETL

--- etl.py
+++ etl.py
@@ -2,15 +2,34 @@ import pandas as pd
 import os
 import glob
 
+
 # uma função de extract que lê e consolida os json
 
-pasta = 'data'
-arquivos_json = glob.glob(os.path.join(pasta, '*json'))
-df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
-df_total = pd.concat(df_list, ignore_index=True)
+def extrair_dados_e_consolidar(pasta: str) -> pd.DataFrame:
+    arquivos_json = glob.glob(os.path.join(pasta, '*json'))
+    df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
+    df_total = pd.concat(df_list, ignore_index=True)
+    return df_total
 
-print(df_total)
 
 # uma função que transforma
 
+def calcular_kpi_de_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
+    df["Total"] = df["Quantidade"] * df["Venda"]
+    return df
+
+
 # uma função que da load em csv ou parquet
+
+def carregar_dados(df: pd.DataFrame, formato_saida: list):
+    for formato in formato_saida:
+        if formato == 'csv':
+            df.to_csv("dados.csv", index=False)
+        if formato == 'parquet':
+            df.to_parquet("dados.parquet")
+
+
+def pipeline_calcular_kpi_de_vendas_consolidada(pasta: str, formato_de_saida: list):
+    data_frame = extrair_dados_e_consolidar(pasta)
+    data_frame_calculado = calcular_kpi_de_total_de_vendas(data_frame)
+    carregar_dados(data_frame_calculado, formato_de_saida)
